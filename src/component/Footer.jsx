644b8a238a4d2ec64f199f9e9f99b05e3cb4b915
musakazim989 <musakazim989@gmail.com>
2023-07-11 21:07:42 +0600

    footer updated

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react"
 function Footer() {
   return (
     <div className=" py-8 bg-slate-800">
-      <h1 className=" text-white font-bold text-center text-[50px]">Footer</h1>
+      <h1 className=" text-white font-bold text-center text-[50px]">
+        All copyright reserved by @Musa Kazim
+      </h1>
     </div>
   )
 }
