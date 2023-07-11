282c1fdcd544553e5eaa53145898b1bf3b8ffab1
musakazim989 <musakazim989@gmail.com>
2023-07-11 21:06:43 +0600

    check button

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -14,23 +14,24 @@ function Content() {
       <div className="w-1/2 flex flex-col px-8">
         <textarea
           onChange={(e) => setShow(e.target.value)}
-          name=""
-          id=""
-          cols="30"
+          type="text"
+          placeholder="Write your text here"
           rows="10"
+          cols="30"
+          className="bg-slate-800 text-white font-bold text-[30px] px-4 py-2"
         ></textarea>
         <br />
         <button
           onClick={showText}
           className="px-4 py-2 bg-blue-500 text-white font-bold"
         >
-          Show
+          Show text
         </button>
       </div>
       <div className="w-1/2 px-8">
         {!check && (
           <h1 className="font-bold text-[30px] text-white text-center">
-            Click show to hide me............
+            Click show button to hide me............
           </h1>
         )}
         {check && <p className="text-white text-center font-bold">{data}</p>}
